876f9a29484a3f2784e709efe4eb597b09795d22
fwr882 <fahimrahman2000@gmail.com>
2018-04-11 18:05:14 -0500

    Added a search page with the title only as well as the path.

--- src/main/java/com/launchcode/Menu/Controllers/HomeController.java
+++ src/main/java/com/launchcode/Menu/Controllers/HomeController.java
@@ -5,12 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class TestController {
+@RequestMapping("search")
+public class HomeController {
 
+    //request path: search
     @RequestMapping(value = "")
-    @ResponseBody
     public String index() {
-        return "This is a Test";
+        return "search/index";
 
     }
 }
